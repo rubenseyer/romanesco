77b89b1313bd441a4aaf206109cfb56ebd81cfa1
rubenseyer <ruben.seyer@gmail.com>
2021-02-03 15:19:31 +0100

    change to real apsw

--- romanesco/__init__.py
+++ romanesco/__init__.py
@@ -1,17 +1,17 @@
 import os
 from flask import Flask
-from supersqlite import SuperSQLite, SuperSQLiteConnection
+import apsw
 import datetime
 from .util import round
 
 app = Flask(__name__)
 
 app.config.update(
-    DATABASE_PATH = os.environ['DATABASE_PATH'],
-    SECRET_KEY = os.environ['SECRET_KEY'],
+    DATABASE_PATH=os.environ['DATABASE_PATH'],
+    SECRET_KEY=os.environ['SECRET_KEY'],
 )
 
-db: SuperSQLiteConnection = SuperSQLite.connect(app.config['DATABASE_PATH'])
+db = apsw.Connection(app.config['DATABASE_PATH'])
 
 app.jinja_env.globals['now'] = datetime.datetime.now
 app.jinja_env.globals['round'] = round
