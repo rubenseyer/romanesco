e56dd30a2cff0bd445efa2a093f5ae9e0f1e7074
rubenseyer <ruben.seyer@gmail.com>
2021-02-05 18:11:18 +0100

    deactivate reloader by default

--- romanesco/__main__.py
+++ romanesco/__main__.py
@@ -1,4 +1,9 @@
 import os
 from . import app
 
-app.run(host=os.environ.get('HOST', 'localhost'), port=int(os.environ.get('PORT', 3000)), debug=True)
+app.run(
+    host=os.environ.get('HOST', 'localhost'),
+    port=int(os.environ.get('PORT', 3000)),
+    debug=True,
+    use_reloader=bool(os.environ.get('USE_RELOADER', False))
+)
