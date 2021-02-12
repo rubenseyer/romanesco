c63bfb589142ab11cfb89aea5d02173e8c397cf2
rubenseyer <ruben.seyer@gmail.com>
2021-02-12 11:00:47 +0100

    fix willys edge case

--- romanesco/model/receipt_formats/willys.py
+++ romanesco/model/receipt_formats/willys.py
@@ -46,7 +46,7 @@ def parse(txt: str) -> (datetime, list[(str, Decimal, Decimal, None)]):
         # Indented
         else:
             # Promo
-            if ':' in seg[1] and len(seg) == 3:
+            if (':' in seg[1] or seg[1].startswith('Prisnedsättning')) and len(seg) == 3:
                 price += parse_decimal(seg[2]) / quantity
             # Addon
             elif '+' in seg[1]:
