73a0d01016d657557f66f5351bab8ff0b1d078d7
rubenseyer <ruben.seyer@gmail.com>
2021-09-18 13:59:24 +0200

    proxy fix

--- romanesco/__init__.py
+++ romanesco/__init__.py
@@ -1,10 +1,14 @@
 import os
-from flask import Flask
-import apsw
 import datetime
+
+from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
+import apsw
+
 from .util import round_even
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1)
 
 app.config.update(
     DATABASE_PATH=os.environ['DATABASE_PATH'],
@@ -16,5 +20,4 @@ db = apsw.Connection(app.config['DATABASE_PATH'])
 app.jinja_env.globals['now'] = datetime.datetime.now
 app.jinja_env.globals['round'] = round_even
 
-#from . import filters
 from . import views
