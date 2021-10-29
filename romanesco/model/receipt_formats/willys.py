80fb872cdb4d6c7ae74aa5203408bd9c17f855a0
rubenseyer <ruben.seyer@gmail.com>
2021-10-29 16:47:10 +0200

    FFFD resistance

--- romanesco/model/receipt_formats/willys.py
+++ romanesco/model/receipt_formats/willys.py
@@ -49,7 +49,7 @@ def parse(txt: str) -> (datetime, list[(str, Decimal, Decimal, None)]):
         # Indented
         else:
             # Promo
-            if (':' in seg[1] or seg[1].startswith('Prisnedsättning') or seg[1].startswith('Nytt pris')) and len(seg) == 3:
+            if (':' in seg[1] or seg[1].startswith('Prisnedsättning') or seg[1].startswith('Nytt pris') or seg[1].startswith('Prisneds\uFFFDttning')) and len(seg) == 3:
                 price += parse_decimal(seg[2]) / quantity
             # Addon
             elif '+' in seg[1]:
