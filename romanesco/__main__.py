2e7baa632d32a328f74039d08462c19f33c44fd0
rubenseyer <ruben.seyer@gmail.com>
2022-08-06 12:11:46 +0200

    bump to py3.10 and deps

--- romanesco/__main__.py
+++ romanesco/__main__.py
@@ -2,37 +2,41 @@ import sys
 import os
 from . import app
 
-host = os.environ.get('HOST', 'localhost')
-port = int(os.environ.get('PORT', 3000))
-use_reloader = bool(os.environ.get('USE_RELOADER', False))
 
-if use_reloader:
-    # use dev server
-    use_bjoern = False
-else:
-    # try to use bjoern if it is installed
-    try:
-        import bjoern
-        use_bjoern = True
-    except ImportError:
+def main():
+    host = os.environ.get('HOST', 'localhost')
+    port = int(os.environ.get('PORT', 3000))
+    use_reloader = bool(os.environ.get('USE_RELOADER', False))
+
+    if use_reloader:
+        # use dev server
         use_bjoern = False
+    else:
+        # try to use bjoern if it is installed
+        try:
+            import bjoern
+            use_bjoern = True
+        except ImportError:
+            use_bjoern = False
 
-if '--single' in sys.argv[1:]:
-    app.config['SINGLE_USER_MODE'] = True
+    if '--single' in sys.argv[1:]:
+        app.config['SINGLE_USER_MODE'] = True
 
-if use_bjoern:
-    from logging import Formatter
-    from flask.logging import default_handler
-    print(f'Romanesco @ {host}:{port}')
-    app.logger.setLevel('INFO')
-    default_handler.setFormatter(Formatter(fmt='%(message)s'))
-    bjoern.run(app, host, port, reuse_port=True)
-else:
-    app.run(
-        host=host,
-        port=port,
-        debug=True,
-        use_reloader=use_reloader
-    )
+    if use_bjoern:
+        from logging import Formatter
+        from flask.logging import default_handler
+        print(f'Romanesco @ {host}:{port}')
+        app.logger.setLevel('INFO')
+        default_handler.setFormatter(Formatter(fmt='%(message)s'))
+        bjoern.run(app, host, port, reuse_port=True)
+    else:
+        app.run(
+            host=host,
+            port=port,
+            debug=True,
+            use_reloader=use_reloader
+        )
 
 
+if __name__ == '__main__':
+    main()
--- romanesco/botccoli/__main__.py
+++ romanesco/botccoli/__main__.py
@@ -5,35 +5,41 @@ from .processor import process_all
 
 from ..model.lookups import users
 
-if len(sys.argv) != 2:
-    print('Usage: botccoli <process|register>')
-    sys.exit(1)
 
-cmd = sys.argv[1]
-if cmd not in ('process', 'register'):
-    print(f'Unrecognized command {cmd}.')
-    sys.exit(2)
+def main():
+    if len(sys.argv) != 2:
+        print('Usage: botccoli <process|register>')
+        sys.exit(1)
 
-if cmd == 'process':
-    process_all()
-    sys.exit(0)
+    cmd = sys.argv[1]
+    if cmd not in ('process', 'register'):
+        print(f'Unrecognized command {cmd}.')
+        sys.exit(2)
 
-# cmd == 'register'
-users_map = users()
-user_id = int(input('Romanesco user id: '))
-if user_id not in users_map:
-    print('Could not find user.')
-    exit(3)
-print(f'  --> {users_map[user_id]}')
+    if cmd == 'process':
+        process_all()
+        sys.exit(0)
 
-avail_adapters = list(adapter_classes().keys())
-adapter_key = input(f'Adapter type (from {",".join(avail_adapters)}): ')
-if adapter_key not in avail_adapters:
-    print('Could not find adapter.')
-    exit(4)
+    # cmd == 'register'
+    users_map = users()
+    user_id = int(input('Romanesco user id: '))
+    if user_id not in users_map:
+        print('Could not find user.')
+        exit(3)
+    print(f'  --> {users_map[user_id]}')
 
-user = input(f'User/login: ')
-passwd = getpass.getpass()
+    avail_adapters = list(adapter_classes().keys())
+    adapter_key = input(f'Adapter type (from {",".join(avail_adapters)}): ')
+    if adapter_key not in avail_adapters:
+        print('Could not find adapter.')
+        exit(4)
 
-register_adapter(user_id, adapter_key, None, None, user, passwd, None)
+    user = input(f'User/login: ')
+    passwd = getpass.getpass()
+
+    register_adapter(user_id, adapter_key, None, None, user, passwd, None)
+
+
+if __name__ == '__main__':
+    main()
 
