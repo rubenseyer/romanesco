0da127e7bae4c21a17c8ea96fe6f24b7d8d1bd77
rubenseyer <ruben.seyer@gmail.com>
2026-07-19 17:47:34 +0200

    Replace bjoern by gunicorn

--- romanesco/__main__.py
+++ romanesco/__main__.py
@@ -8,28 +8,36 @@ def main():
     port = int(os.environ.get('PORT', 3000))
     use_reloader = bool(os.environ.get('USE_RELOADER', False))
 
-    if use_reloader:
-        # use dev server
-        use_bjoern = False
-    else:
-        # try to use bjoern if it is installed
-        try:
-            import bjoern
-            use_bjoern = True
-        except ImportError:
-            use_bjoern = False
-
     if '--single' in sys.argv[1:]:
         app.config['SINGLE_USER_MODE'] = True
 
-    if use_bjoern:
+    if not use_reloader:
         from logging import Formatter
         from flask.logging import default_handler
+        from gunicorn.app.base import BaseApplication
+
+        class RomanescoApplication(BaseApplication):
+            def __init__(self, application, options=None):
+                self.options = options or {}
+                self.application = application
+                super().__init__()
+
+            def load_config(self):
+                for key, value in self.options.items():
+                    if key in self.cfg.settings and value is not None:
+                        self.cfg.set(key.lower(), value)
+
+            def load(self):
+                return self.application
+
         app.logger.info(f'Romanesco @ {host}:{port}')
         app.logger.setLevel('INFO')
         default_handler.setFormatter(Formatter(fmt='%(message)s'))
         try:
-            bjoern.run(app, host, port, reuse_port=True)
+            RomanescoApplication(
+                app,
+                {'bind': f'{host}:{port}', 'reuse_port': True},
+            ).run()
         except KeyboardInterrupt:
             app.logger.warning('Interrupted')
             db.close()
